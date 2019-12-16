aad02a011bb1ca43b1a7643e3d418e261beef237
woilanlan <woilanlan@gmail.com>
2019-12-16 09:12:19 +0800

    1

---
+++ mybatis/ssm03/src/main/java/top/woilanlan/controller/HelloController.java
@@ -0,0 +1,13 @@
+package top.woilanlan.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/index")
+    public String index(){
+        return "index";
+    }
+}
